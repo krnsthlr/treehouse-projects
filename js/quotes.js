d10af1e98ea800fdfa5f0ea6c7b0864c0a0c63bb
Karin Seethaler <karin.seethaler@gmail.com>
2016-11-30 17:37:54 +0100

    Add project files

---
+++ js/quotes.js
@@ -0,0 +1,82 @@
+var quotes = [
+
+	{
+		quote: "Those who believe in telekinetics, raise my hand.",
+		source: "Kurt Vonnegut"
+
+	},
+		{
+		quote: "What the world needs is more geniuses with humility; there are so few of us left.",
+		source: "Oscar Levant",
+		citation: "On the 8th Day - God Laughed"
+	},
+		{
+		quote: "Believe those who are seeking the truth. Doubt those who find it.",
+		source: "André Gide",
+		citation: "So Be It: Or The Chips are Down",
+		year: 1959
+	},
+		{
+		quote: "Don't ever wrestle with a pig. You'll both get dirty, but the pig will enjoy it.",
+		source: "Cale Yarborough"
+
+	},
+		{
+		quote: "In three words I can sum up everything I've learned about life - It goes on.",
+		source: "Rober Frost",
+		citation: "The Harper Book of Quotations"
+	},
+		{
+		quote: "The fool doth think he is wise, but the wise man knows himself to be a fool.",
+		source: "William Shakespeare",
+		citation: "As You Like It",
+	},
+		{
+		quote: "Outside of a dog, a book is a man's best friend. Inside of a dog it's too dark to read.",
+		source: "Groucho Marx",
+		citation: "The Essential Groucho",
+		year: 1974
+	},
+		{
+		quote: "I may not have gone where I intended to go, but I think I have ended up where I needed to be.",
+		source: "Douglas Adams",
+		citation:"The Long Dark Tea-Time of the Soul",
+		year: 1988
+	},
+		{
+		quote: "Never put off till tomorrow what may be done day after tomorrow just as well.",
+		source: "Mark Twain",
+		year: 1870
+	},
+		{
+		quote: "Whenever I feel the need to exercise, I lie down until it goes away.",
+		source: "Paul Terry"
+	},
+		{
+		quote: "Logic will get you from A to Z; imagination will get you everywhere.",
+		source: "Albert Einstein"
+	},
+		{
+		quote: "Reality continues to ruin my life.",
+		source: "Bill Watterson",
+		citation: "The Complete Calvin and Hobbes"
+	},
+		{
+		quote: "I love mankind ... it's people I can't stand.",
+		source: "Charles M. Schulz",
+		citation: "The Complete Peanuts",
+		year: 1959
+	},
+		{
+		quote: "The trouble with having an open mind is that people will insist on coming along and trying to put things in it.",
+		source: "Terry Pratchett",
+		citation: "Diggers",
+		year: 1990
+	},
+		{
+		quote: "The saddest aspect of life right now is that science gathers knowledge faster than society gathers wisdom.",
+		source: "Isaac Asimov"
+	}
+
+
+];
