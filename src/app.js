4abee676a1250acbab70ede59bc57dfc4ed59784
Karin Seethaler <karin.seethaler@gmail.com>
2017-03-24 15:37:25 +0100

    Basic API calls for user timeline and friends. Rendering info to index template

--- src/app.js
+++ src/app.js
@@ -5,23 +5,48 @@ const Twit = require('twit');
 const config = require('../config.js');
 
 const app = express();
-
 const T = new Twit(config);
-
-/**
-T.get('friends/list', {count: '5', include_user_entities: 'false'}, function(err, data, res) {
-	console.log(data);
-}) **/
-
-app.use('/static', express.static(__dirname + '/public'));
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
-app.get('/', function(req, res) {
-	res.render('index');
-})
+app.use('/static', express.static(__dirname + '/public'));
+
+const extractTweets = (tweets) => {
+	return tweets.map((elem) => {
+		return {
+				avatar: elem.user.profile_image_url,
+				name: elem.user.name,
+				screen_name: elem.user.screen_name,
+				text: elem.text,
+				rts: elem.retweet_count,
+				likes: elem.favorite_count
+			}
+	});
+};
+
+const extractFriends = (friends) => {
+	return friends.map((elem) => {
+		return {
+				avatar: elem.profile_image_url,
+				name: elem.name,
+				screen_name: elem.screen_name
+			}
+	});
+};
+
+const getTweets = () => T.get('statuses/user_timeline', {count: '5', include_rts: 'true', exclude_replies: 'true'})
+						.then((result) => extractTweets(result.data));
+
+const getFriends = () => T.get('friends/list', {count: '5', include_user_entities: 'false'})
+						.then((result) => extractFriends(result.data.users));
+
+app.get('/', (req,res) => {
+	Promise.all([getTweets(), getFriends()])
+			.then(([tweets, friends]) => res.render('index', {tweets: tweets, friends: friends}));
+});
+
 
 app.listen(3000, function() {
 	console.log('App listening on port 3000!');
-})
+});
