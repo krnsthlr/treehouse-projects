33eb9be7297d3a918bbfeac60ca1559bf25f3ddd
Karin Seethaler <karin.seethaler@gmail.com>
2017-04-11 17:20:14 +0200

    Add clean task and script task outline

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,34 @@
 'use strict';
 
-const gulp = require('gulp');
+const gulp = require('gulp'),
+	   del = require('del'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	rename = require('gulp-rename'),
+	  maps = require('gulp-sourcemaps'),
+	useref = require('gulp-useref');
+
+const options = {
+	src: 'src',
+	dist: 'dist'
+}
+
+gulp.task('clean', () => {
+	del('dist');
+});
+
+gulp.task('scripts', ['clean'], () => {
+	return gulp.src([options.src + '/js/circle/*.js',
+		options.src + '/js/global.js'])
+		.pipe(maps.init())
+		.pipe(concat('all.min.js'))
+		.pipe(uglify())
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest(options.dist + '/scripts'));
+});
+
+gulp.task('html', ['scripts'], () => {
+	return gulp.src(options.src + '/index.html')
+		.pipe(useref())
+		.pipe(gulp.dest(options.dist));
+});
