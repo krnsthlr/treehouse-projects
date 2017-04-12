15c46d3c92c407b7920b43605d5539d65815a9b0
Karin Seethaler <karin.seethaler@gmail.com>
2017-04-12 15:51:19 +0200

    Add complete build functionality

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,15 @@
 
 const gulp = require('gulp'),
 	   del = require('del'),
-	concat = require('gulp-concat'),
-	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),	  
+	  sass = require('gulp-sass'),
+	  csso = require('gulp-csso'),
+  imagemin = require('gulp-imagemin'),
 	  maps = require('gulp-sourcemaps'),
-	useref = require('gulp-useref');
+	inject = require('gulp-inject'),
+	runSeq = require('run-sequence');
 
 const options = {
 	src: 'src',
@@ -17,9 +21,8 @@ gulp.task('clean', () => {
 	del('dist');
 });
 
-gulp.task('scripts', ['clean'], () => {
-	return gulp.src([options.src + '/js/circle/*.js',
-		options.src + '/js/global.js'])
+gulp.task('scripts', () => {
+	return gulp.src(options.src + '/js/**')
 		.pipe(maps.init())
 		.pipe(concat('all.min.js'))
 		.pipe(uglify())
@@ -27,8 +30,38 @@ gulp.task('scripts', ['clean'], () => {
 		.pipe(gulp.dest(options.dist + '/scripts'));
 });
 
-gulp.task('html', ['scripts'], () => {
-	return gulp.src(options.src + '/index.html')
-		.pipe(useref())
+gulp.task('compileSass', () => {
+	return gulp.src(options.src + '/sass/global.scss')
+		.pipe(maps.init())
+		.pipe(sass())
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest(options.src + '/css'));
+});
+
+gulp.task('styles', ['compileSass'], () => {
+	return gulp.src(options.src + '/css/global.css')
+		.pipe(maps.init({loadMaps: true}))
+		.pipe(csso())
+		.pipe(rename('all.min.css'))
+		.pipe(maps.write('./'))
+		.pipe(gulp.dest(options.dist + '/styles'));
+});
+
+gulp.task('images', () => {
+	return gulp.src(options.src + '/images/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest(options.dist + '/images'));
+});
+
+gulp.task('index', () => {
+	const target = gulp.src(options.src + '/index.html');
+	const sources = gulp.src([options.dist + '/scripts/*', 
+		options.dist + '/styles/*']);
+
+	return target.pipe(inject(sources, {ignorePath: options.dist, addRootSlash: false}))
 		.pipe(gulp.dest(options.dist));
+});
+
+gulp.task('build', () => {
+	runSeq('clean', ['scripts', 'styles', 'images'], 'index');
 });
