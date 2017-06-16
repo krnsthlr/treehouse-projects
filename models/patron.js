7ba51279ba38360a7270f5416212d462a5b69333
Karin Seethaler <karin.seethaler@gmail.com>
2017-06-16 15:55:06 +0200

    Add patrons detail page

--- models/patron.js
+++ models/patron.js
@@ -10,7 +10,7 @@ module.exports = function(sequelize, DataTypes) {
   }, {
     classMethods: {
       associate: function(models) {
-        // associations can be defined here
+         Patron.hasMany(models.Loan, {foreignKey: 'patron_id'});
       }
     }
   });
--- routes/patrons.js
+++ routes/patrons.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 var Patron = require('../models').Patron;
+var Loan = require('../models').Loan;
+var Book = require('../models').Book;
 
 /* GET patrons listing */
 router.get('/', function(req, res, next){
@@ -12,5 +14,24 @@ router.get('/', function(req, res, next){
 	});
 });
 
+/* GET individual patron */
+router.get('/:id', function(req, res, next){
+	Patron.findById(req.params.id, {
+		include: {
+			model: Loan,
+			include: {
+				model: Book
+			}
+		}
+	}).then(patron => {
+		if(patron) {
+			res.render('patrons/detail', {patron: patron});	
+		} else {
+			res.send(404);
+		}
+	}).catch(error => {
+		res.send(500);
+	});
+});
 
 module.exports = router;
